e00a843cf7207a6a11d65c9a5aea34300eb47cc1
Soroosh Heidarpour <sorooshheidarpour@outlook.com>
2024-07-08 19:08:03 +0330

    Added EntityFreamwrok and Created Database

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address {  get; set; }
+        public double Rating { get; set; }
+        public int CountryId {  get; set; }
+        [ForeignKey(nameof(CountryId))]
+        public Country Country { get; set; }
+    }
+}
